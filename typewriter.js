f93f7d22278a3588b0bc97da8fa790c4ec4e4ed4
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-07 16:45:51 -0500

    print each character after a 1 second delay

--- typewriter.js
+++ typewriter.js
@@ -1,6 +1,8 @@
 const sentence = "hello there from lighthouse labs";
 
 for (const char of sentence) {
-  process.stdout.write(char);
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 1000);
 }
 
